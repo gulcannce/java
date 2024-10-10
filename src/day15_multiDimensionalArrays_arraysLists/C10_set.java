a84a1f454c73293d3bb06d25d9ef2de420965493
gulcannce <gulcanncee@gmail.com>
2024-10-10 20:14:05 +0300

    6.proje

--- src/day15_multiDimensionalArrays_arraysLists/C10_set.java
+++ src/day15_multiDimensionalArrays_arraysLists/C10_set.java
@@ -1,4 +1,40 @@
 package day15_multiDimensionalArrays_arraysLists;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class C10_set {
+
+    public static void main(String[] args) {
+
+        List<String> isimler = new ArrayList<>();
+
+        isimler.add("Deniz");
+        isimler.add("omer");
+        isimler.add("Esra");
+        isimler.add("Hamza");
+        isimler.add("Basak");
+
+        System.out.println(isimler); //[Deniz, omer, Esra, Hamza, Basak]
+
+        System.out.println(isimler.get(1)); //omer
+         //System.out.println(isimler.get(8)); // IndexOutOfBoundsException
+
+        isimler.add(3,"Azim");
+
+        System.out.println(isimler); //[Deniz, omer, Esra, Azim, Hamza, Basak]
+         //set()'u bir elementi UPDATE (yeni deger atamak) için kullanılır
+
+        isimler.set(3,"Gulnur");
+        System.out.println(isimler); //[Deniz, omer, Esra, Gulnur, Hamza, Basak]
+
+        //Method'ların yaptıgı iş ile bixe döndürdüğü sonuc FARKLI OLABİLİR
+        System.out.println(isimler.set(2, "Yusuf")); //Esra
+
+        System.out.println(isimler); //[Deniz, omer, Yusuf, Gulnur, Hamza,
+
+        isimler.set(0,"Ayça");
+
+        System.out.println(isimler); //[Ayça, omer, Yusuf, Gulnur, Hamza, Basak]
+    }
 }
--- src/day15_multiDimensionalArrays_arraysLists/C11_remove.java
+++ src/day15_multiDimensionalArrays_arraysLists/C11_remove.java
@@ -1,4 +1,80 @@
 package day15_multiDimensionalArrays_arraysLists;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class C11_remove {
+    public static void main(String[] args) {
+
+        List<String> isimler = new ArrayList<>();
+
+        isimler.add("Deniz");
+        isimler.add("omer");
+        isimler.add("Esra");
+        isimler.add("Hamza");
+        isimler.add("Basak");
+
+        //remove (silinecekObject) çalıştırınca
+        //silinecekObject varsa siler ve bize true döndürür
+        //silinecekObject yoksa silemez ve bize false döndürür
+
+        System.out.println(isimler); //[Deniz, omer, Esra, Hamza, Basak]
+
+        System.out.println(isimler.remove("Esra")); //true
+
+        System.out.println(isimler); //[Deniz, omer, Hamza, Basak]
+
+        System.out.println(isimler.remove("Ercan")); //false
+
+        System.out.println(isimler); //[Deniz, omer, Hamza, Basak]
+
+        isimler.remove("Omer");
+
+        System.out.println(isimler);//[Deniz, Hamza, Basak]
+
+        //remove (silinecekIndex) çalıştırınca,
+        //silinecekObject varsa siler ve bize true döndürür
+        //silinecekObject yoksa silemez ve bize false döndürür
+
+        System.out.println(isimler.remove(1)); //Hamza
+
+        System.out.println(isimler); //[Deniz, Basak]
+
+        //isimlr.remove(5); //IndexOfBoundsException
+
+
+        isimler.add("Ibrahim");
+        isimler.add("Ercan");
+        isimler.add("Azim");
+
+        System.out.println(isimler); //[Deniz, Hamza, Basak, Ibrahim, Ercan, Azim]
+
+        List<String> silinecekler = new ArrayList<>();
+        silinecekler.add("Basak");
+        silinecekler.add("Deniz");
+        silinecekler.add("Azim");
+
+        System.out.println(silinecekler); //[Basak, Deniz, Azim]
+
+        System.out.println(isimler.removeAll(silinecekler)); //true
+        System.out.println(isimler); //[İbrahim,Ercan]
+
+        List<String> silinecekler2 = new ArrayList<>();
+        silinecekler2.add("Ercan");
+        silinecekler2.add("Gulnur");
+
+        System.out.println(); // [Ercan, Gulnur]
+
+        System.out.println(isimler.removeAll(silinecekler2)); //true
+        System.out.println(isimler); //[İbrahim]
+
+        System.out.println(isimler.removeAll(silinecekler)); // false
+        System.out.println(isimler); //[İbrahim]
+
+
+
+
+
+
+    }
 }
